8877a8f03f094531841797f6b99e6e59778d2e3e
shanyouli <shanyouli6@gmail.com>
2019-06-27 13:51:24 +0800

    Change the theme over time 	modified:   lisp/init-theme.el

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,31 +1,8 @@
-;;; init-theme.el --- Initialize theme configuration  -*- lexical-binding: t; -*-
+;;; init-theme.el -- Initialize theme configuration -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019  lye li
+;;; Theme
 
-;; Author: lye li <shanyouli6@gmail.com>
-;; Keywords:theme
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;;
-
-;;; Code:
-;; https://github.com/honmaple/dotfiles/blob/master/emacs.d/site-lisp/maple/maple-theme.el
-
-;; Theme
+;; Required plugin package
 (use-package doom-themes
   :defines (doom-themes-enable-blod doom-themes-enable-italic)
   :config
@@ -33,67 +10,35 @@
         doom-themes-enable-italic t)   ; enable italic
   (doom-themes-org-config) ; org-mode
   )
-(use-package zenburn-theme)
-(use-package material-theme)
-(use-package dracula-theme)
 
+;; variable
+(defvar lye-light-theme nil
+  "Bright color theme used.")
+(defvar lye-dark-theme nil
+  "Dark theme used.")
 
-(defvar lye-emacs-theme-list '(doom-molokai
-                               doom-one
-                               zenburn
-                               material
-                               dracula)
-  "Some theme cycle!")
+;; functions
+;;@see https://emacs-china.org/t/emacs-theme/7781
+(defun exchange-bright-and-dark-theme ()
+  "Use a specific topic for a specific time period."
+  (let ((hour (string-to-number (substring (current-time-string) 11 13)))
+        (current-theme (car custom-enabled-themes))
+        (now-theme nil))
+    (if (member hour (number-sequence 8 19))
+        (setq now-theme lye-light-theme)
+      (setq now-theme lye-dark-theme))
+    (when now-theme
+      (let ((progress-reporter
+             (make-progress-reporter
+              (format "Loading theme %s..." now-theme))))
+        (mapc 'disable-theme custom-enabled-themes)
+        (load-theme now-theme t)
+        (progress-reporter-done progress-reporter)))))
 
-(defvar lye-emacs-theme 'doom-one)
-
-(defun lye/theme-cycle (&optional step)
-  "Loop emacs theme!"
-  (let ((next-theme lye-emacs-theme-list))
-    (if step
-        (progn
-          (if (memq (car custom-enabled-themes) next-theme)
-              (progn
-                (setq next-theme (if (< step 0)
-                                     (reverse lye-emacs-theme-list)
-                                   lye-emacs-theme-list))
-
-                (setq lye-emacs-theme
-                      (or (cadr (member lye-emacs-theme next-theme))
-                          (car next-theme)))
-                (when lye-emacs-theme
-                  (let ((progress-reporter
-                         (make-progress-reporter
-                          (format "Loading theme %s..." lye-emacs-theme))))
-                    (mapc 'disable-theme custom-enabled-themes)
-                    (load-theme lye-emacs-theme t)
-                    (progress-reporter-done progress-reporter))))
-            (message "The current theme does not belong to one of lye-emacs-theme-list.
- To loop, add it to lye-emacs-theme-list.")))
-
-      ;; (substring (format "%s" custom-enabled-themes) 1 -1)
-      (message "The Current theme is %s" (car custom-enabled-themes))
-      )))
-
-;; Understand the topics currently in use
-(defun lye/current-theme ()
-  "what is the Current theme?"
-  (interactive)
-  (lye/theme-cycle))
-
-(defun lye/next-theme ()
-  "Netx theme!"
-  (interactive)
-  (lye/theme-cycle 1))
-
-(defun lye/previous-theme ()
-  "Previous theme."
-  (interactive)
-  (lye/theme-cycle -1))
-
-(if (display-graphic-p)
-    (load-theme lye-emacs-theme t)
-  (require 'lazycat-theme))
+(when (display-graphic-p)
+  (setq lye-light-theme 'doom-solarized-light)
+  (setq lye-dark-theme 'doom-one)
+  (run-with-timer 0 3600 'exchange-bright-and-dark-theme))
 
 (provide 'init-theme)
 ;;; init-theme.el ends here
