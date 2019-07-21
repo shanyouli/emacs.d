c629c3aea9af2a6a35c786b9b2f38d94ff4276b6
shanyouli <shanyouli@gmail.com>
2019-07-21 21:39:10 +0800

    More precise exchange of topic functions over time

--- core/core-third-package.el
+++ core/core-third-package.el
@@ -30,12 +30,13 @@
 
 ;; Setup `use-package'
 ;; Should set before loading `use-package'
-(eval-when-compile (require 'use-package))
-(eval-and-compile
-  (setq use-package-always-ensure t)
-  (setq use-package-always-defer t)
-  (setq use-package-expand-minimally t)
-  (setq use-package-enable-imenu-support t))
+(eval-when-compile
+  (progn
+    (require 'use-package)
+    (setq use-package-always-ensure t)
+    (setq use-package-always-defer t)
+    (setq use-package-expand-minimally t)
+    (setq use-package-enable-imenu-support t)))
 
 ;; Extensions
 (use-package diminish :ensure nil)
