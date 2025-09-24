28fb3c6c1aa0b72e3221b5f71e3d35bd67f8b5c3
Shanyou Li <shanyouli6@gmail.com>
2025-09-24 21:48:16 +0800

    feat: add lib

---
+++ lib/auto-space.el
@@ -0,0 +1,45 @@
+;;; lib/auto-space.el -*- lexical-binding: t; -*-
+
+;; 可以使用零宽字符, "\u200B"
+(defvar my-space-char " " "默认空格格式，")
+
+(defun my-add-space-between-chinese-and-english ()
+  "在中英文之间自动添加空格。"
+  (let ((current-char (char-before))
+        (prev-char (char-before (1- (point)))))
+    (when (and current-char prev-char
+               (or (and (my-is-chinese-character prev-char) (my-is-halfwidth-character current-char))
+                   (and (my-is-halfwidth-character prev-char) (my-is-chinese-character current-char)))
+               (not (eq prev-char ?\s))) ; 检查前一个字符不是空格
+      (save-excursion
+        (goto-char (1- (point)))
+        (insert my-space-char)))))
+
+(defun my-is-chinese-character (char)
+  "判断字符是否为中文字符。"
+  (and char (or (and (>= char #x4e00) (<= char #x9fff))
+                (and (>= char #x3400) (<= char #x4dbf))
+                (and (>= char #x20000) (<= char #x2a6df))
+                (and (>= char #x2a700) (<= char #x2b73f))
+                (and (>= char #x2b740) (<= char #x2b81f))
+                (and (>= char #x2b820) (<= char #x2ceaf)))))
+
+(defun my-is-halfwidth-character (char)
+  "判断字符是否为半角字符，包括英文字母、数字和标点符号。"
+  (and char (or (and (>= char ?a) (<= char ?z))
+                (and (>= char ?A) (<= char ?Z))
+                (and (>= char ?0) (<= char ?9))
+                )))
+
+(defun my-delayed-add-space-between-chinese-and-english ()
+  "延迟执行，在中英文之间自动添加空格。"
+  (run-with-idle-timer 0 nil 'my-add-space-between-chinese-and-english))
+
+;;;###autoload
+(define-minor-mode my-auto-space-mode
+  "在中英文之间自动添加空格的模式。"
+  :lighter " Auto-Space"
+  :global t
+  (if my-auto-space-mode
+      (add-hook 'post-self-insert-hook 'my-add-space-between-chinese-and-english)
+    (remove-hook 'post-self-insert-hook 'my-add-space-between-chinese-and-english)))
---
+++ lib/initframe.el
@@ -0,0 +1,74 @@
+;;; lib/initframe.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun initframe-get-display-info (&optional name)
+  "获取 emacs 所在显示器大小，当 `NAME' 为 width 表示获取 width，
+为 `height' 获取 height。 默认获取 width 和 height"
+  (let ((infos (assq 'geometry (frame-monitor-attributes))))
+    (cond ((eq name 'width) (nth 3 infos))
+          ((eq name 'height) (nth 4 infos))
+          (t (last infos 2)))))
+
+;;;###autoload
+(defun initframe-init-default-frame (&optional frame wratio hratio)
+  "根据比例设置窗口 size，`WRATIO' 表示窗口宽度占显示器的比例，
+`HRATIO' 表示窗口高度占显示器的高度比例。
+`FRAME'是否指定窗口，默认为当期聚焦窗口."
+  (interactive)
+  (let* ((display-size (initframe-get-display-info))
+         (x-width (if wratio
+                       (truncate (- (* (car display-size) wratio) 20))
+                     (or (alist-get 'width default-frame-alist)
+                         (truncate (- (* (car display-size) 0.5) 20)))))
+         (y-height (if hratio
+                       (truncate (- (* (cadr display-size) hratio)))
+                     (or (alist-get 'height default-frame-alist)
+                         (truncate (- (* (cadr display-size) 0.5)))))))
+    (set-frame-size (or frame (selected-frame)) x-width y-height (>= width 200))))
+
+;; EmacsClient 配置
+(defvar initframe-client-frame nil "Store the generated EmacsClient frame")
+(defconst initframe-client-frame-prefix-name "EmacsClient" "指定emacs client 名称。")
+
+(defvar initframe-client-frame-parameters
+  `((name . ,initframe-client-frame-prefix-name)
+    (width . 88)
+    (height . 27)
+    (transient . t)
+    ,@(when IS-MAC `((window-system . ns)
+                     (menu-bar-lines . 1)))
+    ,@(when IS-LINUX
+        `((window-system . ,(if (boundp 'pgtk-initialized) 'pgtk 'x))
+          (display . ,(or (getenv "WAYLAND_DISPLAY")
+                          (getenv "DISPLAY")
+                          ":0")))))
+  "emacs client 默认 frame 参数")
+
+(defun initframe-frame-is-client-frame-p (&optional frame)
+  "Return t 如果当前 frame 就是 Emacs-Client"
+  (string-prefix-p initframe-client-frame-prefix-name (frame-parameter frame 'name)))
+
+(defun initframe-client-frame-is-live-p ()
+  "判断是否存在以 EmacsClient 开头的 frame 存活。"
+  (if (and initframe-client-frame (frame-live-p initframe-client-frame))
+    initframe-client-frame
+    (setq initframe-client-frame (cl-find-if (lambda (f) (initframe-frame-is-client-frame-p f))
+                                             (frame-list)))
+    initframe-client-frame))
+
+
+;;;###autoload
+(defun initframe-open-client-frame (&optional $file)
+  "打开或聚焦到 client-frame。如果存在路径，则打开该文件。"
+  (let* ((frame-title-format "")
+         (frame (or (initframe-client-frame-is-live-p)
+                    (make-frame initframe-client-frame-parameters))))
+    (if frame
+        (progn
+          (select-frame-set-input-focus frame)
+          (when (and $file (file-exist-p $file))
+            (with-selected-frame frame (find-file $file)))
+          t)
+      nil)))
+
+(provide 'initframe)
