1a0f6c732e99947a7cdf809d642efc00eddc0c6d
Shanyou Li <shanyouli6@gmail.com>
2026-03-31 14:10:12 +0800

    feat: update lib/scratch.el

--- lib/scratch.el
+++ lib/scratch.el
@@ -12,7 +12,7 @@
      :extension "py"
      :entry "main.py"
      :shebang "#!/usr/bin/env python")
-    (javascript :init ("bun" "init" "-y") :extension "js" :entry "index.js")
+    (javascript :init ("bun" "init" "-y") :extension "js" :entry "index.js" :pre t)
     (typescript :init ("bun" "init" "-y") :extension "ts" :entry "index.ts")
     (bash :init nil :extension "sh" :shebang "#!/usr/bin/env bash"))
   "配置列表: 语言 (:init 初始化命令列表 :extension 后缀 :entry 入口文件)")
@@ -23,13 +23,16 @@
          (if (stringp (car commands))
              (list commands)
            commands))
-        (success nil))
-    (cl-loop
-     for cmd in cmds when (executable-find (car cmd)) do
-     (progn
-       (apply #'call-process (car cmd) nil nil (cdr cmd))
-       (setq success t))
-     until success)
+        (success nil)
+        (temp-buffer (get-buffer-create "*scratch-init-error*"))
+        )
+    (cl-loop for cmd in cmds
+             when (executable-find (car cmd))
+             do
+             (progn
+               (apply #'call-process (car cmd) nil temp-buffer nil (cdr cmd))
+               (setq success t))
+             until success)
     success))
 
 (defun my-scratch-create-new-entry (path &optional shebang)
@@ -66,6 +69,7 @@
                    "test." (or (plist-get config :extension) lang-input))))
              (lang-root (expand-file-name lang-input my-scratch-root-dir))
              (init-commands (plist-get config :init))
+             (pre-is-ok (plist-get config :pre))
              (entry-path (expand-file-name entry-name lang-root))
              (today (format-time-string "%Y%m%d"))
              (default-directory lang-root))
@@ -86,27 +90,31 @@
                    (format "git add %s"
                            (shell-quote-argument
                             (format "%s/%s-%s"
-                                    archive-path file-date entry-name))))
+                                    archive-path file-date entry-name)))
+                   nil)
                   (shell-command
-                   (format "git commit -m 'Archive %s'" file-date))
+                   (format "git commit -m 'Archive %s'" file-date) nil)
                   (my-scratch-create-new-entry entry-path
                                                (plist-get config :shebang)))))
           (make-directory lang-root t)
           (let ((default-directory lang-root))
             ;; 使用专业工具对项目进行初始化
+            (when pre-is-ok
+              (my-scratch-create-new-entry
+               (expand-file-name entry-name lang-root)
+               (plist-get config :shebang)))
             (when init-commands
               (my-scratch-exec-first-available init-commands))
             (unless (file-exists-p (expand-file-name ".git" lang-root))
-              (shell-command "git init")
+              (shell-command "git init" nil)
               (with-temp-file ".gitignore"
                 (insert "__pycache__/\nnode_modules/\n.venv/\n")))
             (unless (file-exists-p entry-path)
               (my-scratch-create-new-entry entry-path
                                            (plist-get config :shebang)))
             ;; 进行第一次 git commit
-            (shell-command "git add .")
-            (shell-command
-             (format "git commit -m 'First commit on %s'" today))))
+            (shell-command "git add ." nil)
+            (shell-command (format "git commit -m 'First commit on %s'" today) nil)))
         (find-file entry-path)
         (message "scratch by %s" lang-input)))))
 
